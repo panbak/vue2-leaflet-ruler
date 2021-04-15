3f6643d39c4772ed9c5fb4a3fe0749b9600e75c2
panbak <pan22bak@gmail.com>
2021-04-15 17:20:38 +0300

    initial commit

---
+++ LRuler.vue
@@ -0,0 +1,24 @@
+<script>
+import { OptionsMixin, ControlMixin, optionsMerger } from 'vue2-leaflet';
+import 'leaflet-ruler/src/leaflet-ruler';
+import 'leaflet-ruler/src/leaflet-ruler.css';
+import L from 'leaflet';
+
+export default {
+  name: 'LRuler',
+  mixins: [ControlMixin, OptionsMixin],
+  mounted() {
+    const options = optionsMerger(
+      {
+        ...this.controlOptions,
+        options: this.options,
+      },
+      this,
+    );
+    L.control.ruler(options).addTo(this.$parent.mapObject);
+  },
+  render() {
+    return null;
+  },
+};
+</script>
